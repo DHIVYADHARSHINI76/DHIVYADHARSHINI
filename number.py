c088dbea4d486ed5ac62361081f4b00a072dec13
DHIVYADHARSHINI76 <50898060+DHIVYADHARSHINI76@users.noreply.github.com>
2019-05-22 18:28:02 +0630

    Delete number.py
    a=int(input("enter a number"))
    if(a>0):
      print("The given no is positive no")
    elif(a<0):
      print("The given no is negative")
    elif(a==0):
      print("the given no is Zero")
    else:
      print("Enter valid number")

--- number.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter a number"))
-if(a>0):
-  print("The given no is positive no")
-elif(a<0):
-  print("The given no is negative")
-elif(a==0):
-  print("the given no is neither positive nor negative")
-else:
-  print("Enter valid number
