75436c604663fd834b163c60ed7b5fbf5267d3a3
DHIVYADHARSHINI76 <50898060+DHIVYADHARSHINI76@users.noreply.github.com>
2019-05-22 18:23:59 +0630

    Create number.py

---
+++ number.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number"))
+if(a>0):
+  print("The given no is positive no")
+elif(a<0):
+  print("The given no is negative")
+elif(a==0):
+  print("the given no is neither positive nor negative")
+else:
+  print("Enter valid number
